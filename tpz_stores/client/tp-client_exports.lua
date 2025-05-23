0caa28ca4df7d2774ca6e7c8286fb30861acda61
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-24 01:28:34 +0300

    Update tp-client_exports.lua

--- tpz_stores/client/tp-client_exports.lua
+++ tpz_stores/client/tp-client_exports.lua
@@ -3,7 +3,12 @@
 --[[ Exports  ]]--
 -----------------------------------------------------------
 
--- @IsBusy : returns when the player has store menu active.
+-- @param store : Requires the name of a store from Config.Stores.
+exports('OpenStoreByName', function(store)
+    OpenStoreCategoryMenu(store)
+end)
+
+-- @param IsBusy : returns when the player has store menu active.
 exports('IsBusy', function()
     return GetPlayerData().IsBusy
 end)
